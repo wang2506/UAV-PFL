6b9723860cf86a1553bec45929ff30e19302bc6c
wang2506 <wang2506@github.com>
2021-01-14 17:56:32 -0500

    trying 5k iterations as that was when junghoon's looked like improving

---
+++ replot_DRL_checker.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 17:29:18 2021
+
+@author: henry
+"""
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# %% import data
+cwd = os.getcwd()
+
+for i in np.arange(2,5,1):
+    with open(cwd+'/data/'+str(i),'rb') as f:
+        data = pickle.load(f)
+    
+    # plot average reward per 10 iterations
+    data_fixer = [sum(data[j:j+10])/10 for j,jj in enumerate(data) if j % 10 == 0]
+    
+    plt.figure(i)
+    plt.plot(data_fixer[:-1])
+    plt.xlabel('10th iterations instance')
+    plt.ylabel('average reward over the latest 10 iterations')
+    plt.title('final iteration was ' +str(100*i))
+
+
+    min_data = [min(data[j:j+10]) for j,jj in enumerate(data) if j % 10 == 0]
+    
+    
+    plt.figure(10*i)
+    plt.plot(min_data[:-1])
+    plt.xlabel('10th iterations instance')
+    plt.ylabel('min reward over the latest 10 iterations')
+    plt.title('final iteration was ' +str(100*i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
