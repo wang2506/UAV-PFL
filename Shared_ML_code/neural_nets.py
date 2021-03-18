0d5341b1d8e2e4a334177628282999d20891c0d6
wang2506 <wang2506@github.com>
2021-03-18 13:47:44 -0400

    revert to prev version with negative sign

--- Shared_ML_code/neural_nets.py
+++ Shared_ML_code/neural_nets.py
@@ -477,10 +477,9 @@ class LocalUpdate_HF_PFL(object): #MLP 1e-3; CNN 1e-4
             
             # cannot use torch.optim.SGD because this grad updates original params
             optim_plus2 = SGD_HN_PFL_del(net.parameters(),deepcopy(temp_params),\
-                            del_acc=self.lr1*self.lr2/(2*self.del_acc),\
+                            del_acc=-self.lr1/(2*self.del_acc),\
                         momentum=0.5,weight_decay=1e-4)
-            # *self.bs removed from the denominator
-            # try removal of the negative sign
+            # -self.lr1*self.lr2/(2*self.del_acc*self.bs)
                 
             # optim_plus2 = SGD_HN_PFL_del(net.parameters(),deepcopy(temp_params),\
             #                 del_acc=self.lr1/(2*self.del_acc),\
