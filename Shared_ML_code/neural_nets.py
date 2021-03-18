95e8b2c0361abca621054b1bf57557622e8ea51b
wang2506 <wang2506@github.com>
2021-03-18 14:28:22 -0400

    a

--- Shared_ML_code/neural_nets.py
+++ Shared_ML_code/neural_nets.py
@@ -354,7 +354,7 @@ class LocalUpdate_FO_PFL(object):
 
 
 class LocalUpdate_HF_PFL(object): #MLP 1e-3; CNN 1e-2
-    def __init__(self,device,bs,lr1,lr2,epochs,dataset=None,indexes=None,del_acc=1e-3):
+    def __init__(self,device,bs,lr1,lr2,epochs,dataset=None,indexes=None,del_acc=1e-2):
         self.device = device
         self.bs = bs
         self.lr1 = lr1
