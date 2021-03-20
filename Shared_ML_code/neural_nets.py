2f4d84ba5eca379cb897d01c1ee352dd9c4fb2d2
wang2506 <wang2506@github.com>
2021-03-20 13:23:59 -0400

    3

--- Shared_ML_code/neural_nets.py
+++ Shared_ML_code/neural_nets.py
@@ -634,7 +634,7 @@ class LocalUpdate_HF_PFL(object): #MLP 1e-3; CNN 1e-2
 
 
 class LocalUpdate_trad_HF(object): #MLP 1e-3; CNN 1e-2
-    def __init__(self,device,bs,lr1,lr2,epochs,dataset=None,indexes=None,del_acc=1e-3):
+    def __init__(self,device,bs,lr1,lr2,epochs,dataset=None,indexes=None,del_acc=1e-6):
         self.device = device
         self.bs = bs
         self.lr1 = lr1
@@ -755,7 +755,7 @@ class LocalUpdate_trad_HF(object): #MLP 1e-3; CNN 1e-2
                     print(temp_inner_pos[-1].grad)
                     print(temp_inner_neg[-1].grad)
                     
-                    input('debug shit enter something')
+                    # input('debug shit enter something')
                     for p1, p2 in enumerate(lr2_result):
                         # completed sum of all params
                         del_sum_grad.append(p2+self.lr2*self.lr1/(2*self.del_acc)\
