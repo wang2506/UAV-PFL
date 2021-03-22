96b445ee7ae1d9b7a9d26fe7e8806170e7dde627
wang2506 <wang2506@github.com>
2021-03-22 12:25:44 -0400

    a

--- hf_pfl_only_ext.py
+++ hf_pfl_only_ext.py
@@ -210,7 +210,7 @@ for save_type in ['extreme']: #,'mild']: #['extreme','mild','iid']:
     cwd = os.getcwd()
     ## setup FL
     nn_style = 'CNN'
-    nn_style = 'MLP'
+    # nn_style = 'MLP'
     if nn_style == 'MLP':
         d_in = 784 #np.prod(dataset_train[0][0].shape)
         d_h = 64
