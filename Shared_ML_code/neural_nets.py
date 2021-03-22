5023955ff3805c649246ca170058ee6844b972ee
wang2506 <wang2506@github.com>
2021-03-22 12:30:07 -0400

    a

--- Shared_ML_code/neural_nets.py
+++ Shared_ML_code/neural_nets.py
@@ -542,7 +542,7 @@ class LocalUpdate_HF_PFL(object): #MLP 1e-3; CNN 1e-2
             
             # cannot use torch.optim.SGD because this grad updates original params
             optim_plus2 = SGD_HN_PFL_del(net.parameters(),deepcopy(temp_params),\
-                            del_acc=-self.lr1*self.lr2/(self.del_acc),\
+                            del_acc=self.lr1*self.lr2/(2*self.del_acc),\
                         momentum=0.5,weight_decay=1e-4)
             # -self.lr1*self.lr2/(2*self.del_acc*self.bs)
             
@@ -608,7 +608,7 @@ class LocalUpdate_HF_PFL(object): #MLP 1e-3; CNN 1e-2
             # print(net.state_dict()['fc2.bias'])
             
             optim_minus2 = SGD_HN_PFL_del(net.parameters(),deepcopy(temp_params),\
-                            del_acc=self.lr1*self.lr2/(self.del_acc),\
+                            del_acc=self.lr1*self.lr2/(2*self.del_acc),\
                         momentum=0.5,weight_decay=1e-4)
             # *self.bs # on the denominator
             # self.lr1*self.lr2/(2*self.del_acc*self.bs)
