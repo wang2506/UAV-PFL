2821406d9321c2978b7f89cec04a6db27944715c
wang2506 <wang2506@frankie.ecn.purdue.edu>
2021-03-25 13:11:35 -0400

    a

--- fl_only_ext.py
+++ fl_only_ext.py
@@ -43,7 +43,7 @@ dataset_test = torchvision.datasets.MNIST('./data/mnist/',train=False,download=F
 # dataset_test = torchvision.datasets.FashionMNIST('./data/fmnist/',train=False,download=False,\
 #                                 transform=transforms.ToTensor())
 
-device = torch.device('cuda:2')
+device = torch.device('cuda:0')
 # device = torch.device('cpu')
 
 # %% filtering the ML data
