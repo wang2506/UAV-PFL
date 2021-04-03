6200db1b71c219da3622cf13b73685300b0aa010
wang2506 <wang2506@github.com>
2021-04-03 14:30:58 -0400

    a

--- ml_replotter.py
+++ ml_replotter.py
@@ -95,6 +95,8 @@ total_pfl_ratios = []
 full_fl_ratios = []
 full_pfl_ratios = []
 
+full_fl_loss_ratios = []
+
 nn_style = 'CNN'
 # nn_style = 'MLP'
 ratio_vec = [1,2,4]#,6,8]
@@ -128,11 +130,21 @@ for ratio in [1,2,4]:#4,6,8]:
                     +'_'+str(swarm_period)+'_'+str(global_period)+'_'+nn_style+'_debug','rb') as f:
             f_pfl_acc = pk.load(f)
         
+        
+        with open(data_loc+'full_fl_loss_'+iid_style+'_'+str(ratio)+'_'+data_source \
+                  +'_'+str(swarm_period)+'_'+str(global_period)+'_'+nn_style+'_debug','rb') as f:
+            f_fl_loss = pk.load(f)
+        
+        full_fl_loss.append(f_fl_loss)
+        
         total_fl_accs.append(fl_acc)
         total_pfl_accs.append(pfl_acc)
         
         full_fl_accs.append(f_fl_acc)
         full_pfl_accs.append(f_pfl_acc)
+    
+    
+    full_fl_loss_ratios.append(full_fl_loss)
     
     total_fl_ratios.append(total_fl_accs)
     total_pfl_ratios.append(total_pfl_accs)
@@ -143,6 +155,51 @@ for ratio in [1,2,4]:#4,6,8]:
 plt.figure(2)
 f2,ax2 = plt.subplots(1,2,figsize=(10,5),dpi=100)#,sharey=True)
 
+# ind = 0
+# for i in range(len(ratio_vec)): #3
+#     if i == 0:
+#         temp_indexes = np.arange(0,40,step=1) #total_fl_ratios[i][0]
+#     elif i == 1:
+#         temp_indexes = np.arange(0,40,step=2)
+#     else:
+#         temp_indexes = np.arange(0,40,step=4)
+#     temp_indexes = np.arange(0,40,step=ratio_vec[i])
+    
+#     # temp_indexes = np.arange(0,40,step=1)
+    
+#     # temp_indexes2 = list(temp_indexes)
+#     # temp_indexes2.append(40)
+#     # temp_indexes = temp_indexes2
+    
+#     if i == 0:
+#         ax2[ind].plot(temp_indexes,total_fl_ratios[i][0],\
+#             label='H-FL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 1$',\
+#             color='forestgreen',linestyle='solid')
+#         ax2[ind].plot(temp_indexes,total_pfl_ratios[i][0],\
+#             label='HN-PFL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 1$',\
+#             color='darkblue',linestyle='solid')
+#     elif i == 1:
+#         ax2[ind].plot(temp_indexes,total_fl_ratios[i][0],\
+#             label='H-FL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 2$',\
+#             color='forestgreen',linestyle='dashed')
+#         ax2[ind].plot(temp_indexes,total_pfl_ratios[i][0],\
+#             label='HN-PFL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 2$',\
+#             color='darkblue',linestyle='dashed')
+#     else:
+#         ax2[ind].plot(temp_indexes,total_fl_ratios[i][0],\
+#             label='H-FL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 4$',\
+#             color='forestgreen',linestyle='dotted')
+#         ax2[ind].plot(temp_indexes,total_pfl_ratios[i][0],\
+#             label='HN-PFL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 4$',\
+#             color='darkblue',linestyle='dotted')
+            
+# ax2[ind].set_title(data_source.upper()+' Performance Accuracy Personalized')
+# ax2[ind].set_ylabel('Accuracy (%)')
+# ax2[ind].set_xlabel('Local Iteration')
+# ax2[ind].grid(True)
+# ax2[ind].legend()
+
+## temporary loss plotter
 ind = 0
 for i in range(len(ratio_vec)): #3
     if i == 0:
@@ -153,39 +210,27 @@ for i in range(len(ratio_vec)): #3
         temp_indexes = np.arange(0,40,step=4)
     temp_indexes = np.arange(0,40,step=ratio_vec[i])
     
-    # temp_indexes = np.arange(0,40,step=1)
-    
-    # temp_indexes2 = list(temp_indexes)
-    # temp_indexes2.append(40)
-    # temp_indexes = temp_indexes2
-    
     if i == 0:
-        ax2[ind].plot(temp_indexes,total_fl_ratios[i][0],\
+        ax2[ind].plot(temp_indexes,full_fl_loss_ratios[i][0],\
             label='H-FL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 1$',\
             color='forestgreen',linestyle='solid')
-        ax2[ind].plot(temp_indexes,total_pfl_ratios[i][0],\
-            label='HN-PFL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 1$',\
-            color='darkblue',linestyle='solid')
     elif i == 1:
-        ax2[ind].plot(temp_indexes,total_fl_ratios[i][0],\
+        ax2[ind].plot(temp_indexes,full_fl_loss_ratios[i][0],\
             label='H-FL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 2$',\
             color='forestgreen',linestyle='dashed')
-        ax2[ind].plot(temp_indexes,total_pfl_ratios[i][0],\
-            label='HN-PFL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 2$',\
-            color='darkblue',linestyle='dashed')
     else:
-        ax2[ind].plot(temp_indexes,total_fl_ratios[i][0],\
+        ax2[ind].plot(temp_indexes,full_fl_loss_ratios[i][0],\
             label='H-FL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 4$',\
             color='forestgreen',linestyle='dotted')
-        ax2[ind].plot(temp_indexes,total_pfl_ratios[i][0],\
-            label='HN-PFL '+r'$\tau_{s}^{\mathsf{L}} = 1$ '+ r'$\tau_{s}^{\mathsf{G}} = 4$',\
-            color='darkblue',linestyle='dotted')
-            
-ax2[ind].set_title(data_source.upper()+' Performance Accuracy Personalized')
-ax2[ind].set_ylabel('Accuracy (%)')
+
+
+ax2[ind].set_title(data_source.upper()+' loss global')
+ax2[ind].set_ylabel('loss val')
 ax2[ind].set_xlabel('Local Iteration')
 ax2[ind].grid(True)
 ax2[ind].legend()
+
+
 
 ind = 1
 for i in range(3): 
