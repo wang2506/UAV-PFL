413f1bbc28f53ecf434323504e50a298ac2da14b
wang2506 <wang2506@github.com>
2021-04-04 16:23:39 -0400

    a

--- hf_pfl_only_ext_old.py
+++ hf_pfl_only_ext_old.py
@@ -258,7 +258,7 @@ for save_type in [settings.iid_style]:
             default_w = pickle.load(f)
         
         # lr,lr2 = 1e-3,1e-2 #CNN
-        lr,lr2 = 1e-3,1e-3
+        lr,lr2 = 1e-3, 1e-3
         # debug lr-rates
         # lr = 2*1e-4
         # lr2 = 1e-2
