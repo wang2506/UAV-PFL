80100a4d0498a204ab3b09577b1597602a498e41
wang2506 <wang2506@github.com>
2021-04-07 00:24:46 -0400

    a

--- hf_pfl_only_ext_old.py
+++ hf_pfl_only_ext_old.py
@@ -271,7 +271,7 @@ for save_type in [settings.iid_style]:
             default_w = deepcopy(global_net.state_dict())
             
         lr,lr2 = 1e-3,1e-2 #CNN
-        # lr,lr2 = 1e-3, 1e-3
+        lr,lr2 = 1e-3, 1e-1
         # debug lr-rates
         # lr = 2*1e-4
         # lr2 = 1e-2
