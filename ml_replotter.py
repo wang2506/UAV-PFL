e4ff69aa4c4b17c87b56467b379e88ba9a69a9d6
wang2506 <wang2506@github.com>
2021-04-13 13:25:22 -0400

    test ordering of multiprocessing

--- ml_replotter.py
+++ ml_replotter.py
@@ -349,7 +349,7 @@ leg2 = ax2[0].legend(h[0::2],l[0::2],bbox_to_anchor=(-0.25,1.02,2.5,0.2),\
 ax2[0].add_artist(leg1)
 plt.subplots_adjust(top=0.8,wspace=0.15,hspace=0.15)
 
-# plt.savefig(cwd+'/ml_plots/mild_'+data_source+'_ovr_swarms.pdf')
+# plt.savefig(cwd+'/ml_plots/mild_'+data_source+'_ovr_swarms.pdf',bbox_inches='tight')
 
 
 # %% plot 3 ratio plots with taus2 = 1
@@ -615,7 +615,7 @@ leg2 = ax2[0].legend(h[0::2],l[0::2],bbox_to_anchor=(-0.25,1.02,2.5,0.2),\
 ax2[0].add_artist(leg1)
 plt.subplots_adjust(top=0.8,wspace=0.15,hspace=0.15)
 
-plt.savefig(cwd+'/ml_plots/mild_'+data_source+'_ovr_global.pdf')
+# plt.savefig(cwd+'/ml_plots/mild_'+data_source+'_ovr_global.pdf',bbox_inches='tight')
 
 
 
