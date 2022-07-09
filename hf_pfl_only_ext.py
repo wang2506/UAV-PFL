53ba43850c47279051b016cdc7badf7399105682
henry <wang2506@purdue.edu>
2022-07-09 17:28:00 -0400

    a

--- hf_pfl_only_ext.py
+++ hf_pfl_only_ext.py
@@ -504,14 +504,15 @@ for save_type in [settings.iid_style]:
         
         for t in range(int(total_time/swarm_period)):
             if settings.data_style == 'mlradio':
-                # if t*swarm_period < 10:
-                #     lr = 1e-2#5e-2
-                # elif t*swarm_period < 20:
-                #     lr = 5e-3#1e-2
-                # elif t*swarm_period < 30:
-                #     lr = 1e-3#5e-3
-                # elif t*swarm_period < 40:
-                #     lr = 5e-4#1e-3
+                if t*swarm_period < 10:
+                    lr = 1e-0
+                elif t*swarm_period < 20:
+                    lr = 1e-1
+                elif t*swarm_period < 30:
+                    lr = 1e-2
+                elif t*swarm_period < 40:
+                    lr = 1e-3
+                lr2 = 1e-1 #lr
             
                 # if t*swarm_period < 10:
                 #     lr = 5e-2
@@ -523,13 +524,13 @@ for save_type in [settings.iid_style]:
                 #     lr = 1e-3
                 # lr2 = 5e-2 #lr
                 
-                if t*swarm_period < 10:
-                    lr = 1e-1
-                elif t*swarm_period < 20:
-                    lr = 1e-2
-                elif t*swarm_period < 40:
-                    lr = 1e-3
-                lr2 = 1e-1 #lr       
+                # if t*swarm_period < 10:
+                #     lr = 1e-1
+                # elif t*swarm_period < 20:
+                #     lr = 1e-2
+                # elif t*swarm_period < 40:
+                #     lr = 1e-3
+                # lr2 = 1e-1 #lr
                 
             # swarm_w = {i:[] for i in range(settings.swarms)}
             # data_processed = {i:0 for i in range(swarms)}
